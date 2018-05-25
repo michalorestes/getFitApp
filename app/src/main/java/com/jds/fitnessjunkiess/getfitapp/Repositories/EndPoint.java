2218336ca91124d8fd1740abf7a76124de8c7e1f
michalorestes <michalorestes@gmail.com>
2018-05-25 22:26:12 +0100

    test floting action button widget

--- app/src/main/java/com/jds/fitnessjunkiess/getfitapp/Repositories/EndPoint.java
+++ app/src/main/java/com/jds/fitnessjunkiess/getfitapp/Repositories/EndPoint.java
@@ -1,5 +1,5 @@
 package com.jds.fitnessjunkiess.getfitapp.Repositories;
 
 public class EndPoint {
-    public static String URL = "http://9eabfc3a.ngrok.io";
+    public static String URL = "http://efa6a5a6.ngrok.io";
 }

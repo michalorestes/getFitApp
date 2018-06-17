fb9c79a885e8e534c1afa3def0442c7b07466b09
michalorestes <michalorestes@gmail.com>
2018-06-17 08:36:31 +0100

    Redesigned workouts list

--- app/src/main/java/com/jds/fitnessjunkiess/getfitapp/Repositories/EndPoint.java
+++ app/src/main/java/com/jds/fitnessjunkiess/getfitapp/Repositories/EndPoint.java
@@ -1,5 +1,5 @@
 package com.jds.fitnessjunkiess.getfitapp.Repositories;
 
 public class EndPoint {
-    public static String URL = "http://58443832.ngrok.io";
+    public static String URL = "http://71003362.ngrok.io";
 }

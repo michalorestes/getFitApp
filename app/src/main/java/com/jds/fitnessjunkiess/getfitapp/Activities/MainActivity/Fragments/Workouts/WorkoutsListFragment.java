9e3e48fa4b167b2bbbc72c53fba6f9373d8d5f7f
michalorestes <michalorestes@gmail.com>
2018-08-05 18:22:57 +0100

    Removed add view box git status

--- app/src/main/java/com/jds/fitnessjunkiess/getfitapp/Activities/MainActivity/Fragments/Workouts/WorkoutsListFragment.java
+++ app/src/main/java/com/jds/fitnessjunkiess/getfitapp/Activities/MainActivity/Fragments/Workouts/WorkoutsListFragment.java
@@ -1,27 +1,20 @@
 package com.jds.fitnessjunkiess.getfitapp.Activities.MainActivity.Fragments.Workouts;
 
-import android.app.Dialog;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.FrameLayout;
 import com.jds.fitnessjunkiess.getfitapp.Activities.MainActivity.Fragments.Workouts.Adapters.WorkoutListRecycleViewAdapter;
 import com.jds.fitnessjunkiess.getfitapp.Activities.MainActivity.Fragments.Workouts.Adapters.WorkoutListViewHolderInterface;
 import com.jds.fitnessjunkiess.getfitapp.Activities.WorkoutViewActivity.WorkoutViewActivity;
-import com.jds.fitnessjunkiess.getfitapp.CustomViews.AddBoxView;
 import com.jds.fitnessjunkiess.getfitapp.DI.DaggerComponents.DaggerWorkoutViewModelFactoryComponent;
 import com.jds.fitnessjunkiess.getfitapp.DI.DaggerComponents.WorkoutViewModelFactoryComponent;
 import com.jds.fitnessjunkiess.getfitapp.DI.DaggerModules.WorkoutViewModelFactoryModule;
@@ -29,23 +22,15 @@ import com.jds.fitnessjunkiess.getfitapp.Entities.Workout;
 import com.jds.fitnessjunkiess.getfitapp.R;
 import com.jds.fitnessjunkiess.getfitapp.ViewModels.Factories.WorkoutViewModelFactory;
 import com.jds.fitnessjunkiess.getfitapp.ViewModels.WorkoutViewModel;
-
-import java.sql.Time;
 import java.util.List;
 import java.util.Objects;
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
 
 public class WorkoutsListFragment extends Fragment
     implements View.OnClickListener, WorkoutListViewHolderInterface {
 
   private RecyclerView recyclerView;
   private WorkoutListRecycleViewAdapter recycleViewAdapter;
-  private AddBoxView addBoxView;
-  private FloatingActionButton actionButton;
-  private InputMethodManager imm;
   private WorkoutViewModel workoutViewModel;
-  private boolean addBoxStatus;
 
   public WorkoutsListFragment() {}
 
@@ -56,7 +41,7 @@ public class WorkoutsListFragment extends Fragment
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    this.addBoxStatus = false;
+
     // TODO: This should be handled with fragment injection
     WorkoutViewModelFactoryComponent workoutViewModelFactoryComponent =
         DaggerWorkoutViewModelFactoryComponent.builder()
@@ -71,8 +56,6 @@ public class WorkoutsListFragment extends Fragment
 
     workoutViewModel.init(7);
     workoutViewModel.getWorkout().observe(this, w -> this.updateWorkoutsList(w, false));
-
-    this.imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
   }
 
   @Override
@@ -80,25 +63,15 @@ public class WorkoutsListFragment extends Fragment
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_workouts_list, container, false);
     this.recyclerView = view.findViewById(R.id.workoutsList);
-    //        this.recyclerView.setHasFixedSize(true);
+
     RecyclerView.LayoutManager recyclerViewLayoutManager = new LinearLayoutManager(getContext());
     this.recyclerView.setLayoutManager(recyclerViewLayoutManager);
 
     this.recycleViewAdapter = new WorkoutListRecycleViewAdapter(this);
     this.recyclerView.setAdapter(recycleViewAdapter);
 
-    this.actionButton = view.findViewById(R.id.floating_action_add_workout);
-    this.actionButton.setOnClickListener(this);
-
-    this.addBoxView = new AddBoxView(Objects.requireNonNull(getContext()));
-    FrameLayout.LayoutParams addBoxLayoutParams =
-        new FrameLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            Gravity.BOTTOM);
-
-    this.addBoxView.setLayoutParams(addBoxLayoutParams);
-    this.addBoxView.getButton().setOnClickListener(this);
+    FloatingActionButton actionButton = view.findViewById(R.id.floating_action_add_workout);
+    actionButton.setOnClickListener(this);
 
     return view;
   }
@@ -111,27 +84,7 @@ public class WorkoutsListFragment extends Fragment
         AddWorkoutDialog dialogTest = new AddWorkoutDialog();
         dialogTest.show(fragmentTransaction, "tag");
         break;
-      case R.id.overlay_layout:
-        removeAddBoxView((FrameLayout) v);
-        break;
-      case R.id.button:
-        this.addNewWorkout(this.addBoxView.getInputText());
-        break;
     }
-  }
-
-  private void addNewWorkout(String workoutName) {
-    Workout workout = new Workout();
-    workout.setName(workoutName);
-    workout.setUserId(7);
-    this.workoutViewModel
-        .addWorkout(workout)
-        .observe(
-            this,
-            w -> {
-              this.workoutViewModel.getWorkout().getValue().add(0, w);
-              this.updateWorkoutsList(this.workoutViewModel.getWorkout().getValue(), true);
-            });
   }
 
   public void updateWorkoutsList(List<Workout> workouts, boolean newWorkout) {
@@ -139,29 +92,6 @@ public class WorkoutsListFragment extends Fragment
     if (newWorkout) {
       recyclerView.smoothScrollToPosition(0);
     }
-  }
-
-  private void addBoxView(FrameLayout viewGroup, View v) {
-    this.addBoxStatus = true;
-    viewGroup.setClickable(true);
-    viewGroup.setOnClickListener(this);
-    viewGroup.setBackgroundColor(
-        getResources().getColor(R.color.cardview_shadow_start_color, null));
-    this.actionButton.setVisibility(View.GONE);
-//    viewGroup.removeView(v);
-    viewGroup.addView(this.addBoxView);
-    this.addBoxView.requestInputFocus();
-    this.imm.showSoftInput(this.addBoxView.getInput(), InputMethodManager.SHOW_IMPLICIT);
-  }
-
-  private void removeAddBoxView(FrameLayout viewGroup) {
-    this.imm.hideSoftInputFromWindow(this.addBoxView.getInput().getWindowToken(), 0);
-    viewGroup.setClickable(false);
-    viewGroup.removeView(this.addBoxView);
-//    this.addBoxView.setVisibility(View.GONE);
-    viewGroup.setBackgroundColor(getResources().getColor(R.color.cardview_shadow_end_color, null));
-    this.addBoxStatus = false;
-    this.actionButton.setVisibility(View.VISIBLE);
   }
 
   @Override
@@ -176,10 +106,5 @@ public class WorkoutsListFragment extends Fragment
 
     workoutView.putExtra("workoutData", workout);
     startActivity(workoutView);
-  }
-
-  public boolean isAddBoxDisplayed()
-  {
-    return this.addBoxStatus;
   }
 }
